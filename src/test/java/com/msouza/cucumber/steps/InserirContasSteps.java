352773dd07372a7a8f179b2e6a2fed447dda081d
Marcos Antonio Gregorio de Souza <marcos.souza@mercadolivre.com>
2021-04-25 15:29:30 -0300

    Fix sleep

--- src/test/java/com/msouza/cucumber/steps/InserirContasSteps.java
+++ src/test/java/com/msouza/cucumber/steps/InserirContasSteps.java
@@ -1,7 +1,6 @@
 package com.msouza.cucumber.steps;
 
 import io.cucumber.java.After;
-import io.cucumber.java.BeforeStep;
 import io.cucumber.java.Scenario;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Então;
@@ -26,37 +25,44 @@ public class InserirContasSteps {
     @Dado("que estou acessando a aplicação")
     public void que_estou_acessando_a_aplicação() {
         driver.get("https://seubarriga.wcaquino.me/");
+        sleep();
     }
 
     @Quando("informo o usuário {string}")
     public void informo_o_usuário(String user) {
         driver.findElement(By.id("email")).sendKeys(user);
+        sleep();
     }
 
     @Quando("a senha {string}")
     public void a_senha(String pass) {
         driver.findElement(By.name("senha")).sendKeys(pass);
+        sleep();
     }
 
     @Quando("seleciono entrar")
     public void seleciono_entrar() {
         driver.findElement(By.tagName("button")).click();
+        sleep();
     }
 
     @Então("visualizo a página inicial")
     public void visualizo_a_pagina_inicial() {
         String texto = driver.findElement(By.xpath("//div[@class='alert alert-success']")).getText();
         Assert.assertTrue(texto.contains("Bem vindo"));
+        sleep();
     }
 
     @Quando("seleciono Contas")
     public void seleciono_contas() {
         driver.findElement(By.linkText("Contas")).click();
+        sleep();
     }
 
     @Quando("seleciono Adicionar")
     public void seleciono_adicionar() {
         driver.findElement(By.linkText("Adicionar")).click();
+        sleep();
     }
 
     @Quando("informo a conta {string}")
@@ -65,29 +71,34 @@ public class InserirContasSteps {
             nome = nome.concat(RandomString.make(10));
         }
         driver.findElement(By.name("nome")).sendKeys(nome);
+        sleep();
     }
 
     @Quando("seleciono Salvar")
     public void seleciono_salvar() {
         driver.findElement(By.tagName("button")).click();
+        sleep();
     }
 
     @Então("a conta é inserida com sucesso")
     public void a_conta_é_inserida_com_sucesso() {
         String texto = driver.findElement(By.xpath("//div[@class='alert alert-success']")).getText();
         Assert.assertEquals("Conta adicionada com sucesso!", texto);
+        sleep();
     }
 
     @Então("sou notificado que o nome da conta é obrigatório")
     public void sou_notificar_que_o_nome_da_conta_e_obrigatorio() {
         String texto = driver.findElement(By.xpath("//div[@class='alert alert-danger']")).getText();
         Assert.assertEquals("Informe o nome da conta", texto);
+        sleep();
     }
 
     @Então("sou notificado que já existe uma conta com esse nome")
     public void sou_notificado_que_ja_existe_uma_conta_com_esse_nome() {
         String texto = driver.findElement(By.xpath("//div[@class='alert alert-danger']")).getText();
         Assert.assertEquals("Já existe uma conta com esse nome!", texto);
+        sleep();
     }
 
     @After
@@ -96,9 +107,12 @@ public class InserirContasSteps {
 		driver.quit();
     }
 
-    @BeforeStep
-    public void beforeStep() throws InterruptedException {
-        TimeUnit.MILLISECONDS.sleep(1000);
+    public void sleep() {
+        try {
+            TimeUnit.MILLISECONDS.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public void screenshot(Scenario scenario) {
